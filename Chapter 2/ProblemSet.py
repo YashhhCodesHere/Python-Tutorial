8f583c6d061e4566a486518a302d959ca2952b2f
Yash Kumar <Yashxhere@gmail.com>
2024-08-13 11:40:06 +0530

    Questions completed

--- Chapter 2/ProblemSet.py
+++ Chapter 2/ProblemSet.py
@@ -1,5 +1,44 @@
+# 1.
+
 a = int(input("Enter the first number: "))
 
 b = int(input("Enter the second number: "))
 
 print("\nThe sum of your entered numbers are: ", a+b)
+
+# 2. 
+
+c = int(input("Enter your number: "))
+z = int(input("Enter the number you divide the above number with: "))
+d = '''This is gonna remains to be a string type variable untill and 
+unless it's get typecasted!'''
+
+print("The remainder is: ", c%z)
+
+# 3. 
+
+# CHECKING THE TYPE OF VARIABLES:
+
+print("The datatype of variable 'a' is: ", type(a))
+print("The datatype of variable z is: ", type(z))
+print("The datatype of variable d is: ", type(d))
+
+# 4. 
+
+x = 34
+y = 80
+
+print("a is greater than b : ", x>y)
+
+# 5. 
+
+m = int(input("Enter first number: "))
+n = int(input("Enter second number: "))
+
+print("Average of your both given numbers are: ", ((m+n)/2))
+
+# 6.
+
+p = int(input("Enter your number which you wanted to be squarred: "))
+
+print("The square of your given number are: ", p*p)
