097bf731c5fd2eada717842ba45b6940d456c133
Yash Kumar <Yashxhere@gmail.com>
2024-08-13 10:58:08 +0530

    Problems

---
+++ Chapter 2/Input.py
@@ -0,0 +1,16 @@
+a = input("Enter your desired first number: ") # Input is taken as string here
+
+# print(type(a))
+
+c = int(a) # So we typecast here the given above string value to integer to make their sum to be possible!
+
+# print(type(c))
+
+print("Your entered first number is: ",a)
+
+b = int(input(" Enter your second number: ")) # Another format of typecasting!
+
+print("Your second number is : ", b)
+
+sum = c + b
+print("Sum of your entered two numbers are: ", sum)
---
+++ Chapter 2/Typecasting.py
@@ -0,0 +1,18 @@
+# TypeFunction:-
+
+j = 199
+i = 31.21
+
+# Shows class below:-
+
+print(type(i))
+print(type(j))
+
+
+# Typecasting:-
+
+a = "31"
+b = float(a)
+print(type(b))
+k = int(19.20)
+print(type(k))
