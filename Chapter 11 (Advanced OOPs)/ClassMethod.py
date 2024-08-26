478051af65c474b6a676c1f336b89163b66fac6a
Yash Kumar <Yashxhere@gmail.com>
2024-08-26 11:56:04 +0530

    feat: Update Worker class to fix name attribute and add instance of Worker

--- Chapter 11 (Advanced OOPs)/ClassMethod.py
+++ Chapter 11 (Advanced OOPs)/ClassMethod.py
@@ -1,6 +1,6 @@
 class Worker:
     salary = 10000
-    name = "Ankur"
+
     def show(self):
         print(f"The salary of the {self.name} is {self.salary}!")
 
@@ -9,6 +9,7 @@ class Worker:
         print(f"The salary of Ankur is {cls.salary}")
 
 Ankur = Worker()
+Ankur.name = "Ankurr"
 Ankur.salary = 15000
 
 print(Ankur.show())     # This will print the salary of Ankur on the basis of instance variable
