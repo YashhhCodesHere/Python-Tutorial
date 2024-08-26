dce64a53418181cca3fa1a58c7195ac82260af72
Yash Kumar <Yashxhere@gmail.com>
2024-08-26 11:45:17 +0530

    feat: Add class method to update salary in Worker class

---
+++ Chapter 11 (Advanced OOPs)/ClassMethod.py
@@ -0,0 +1,15 @@
+class Worker:
+    salary = 10000
+    name = "Ankur"
+    def show(self):
+        print(f"The salary of the {self.name} is {self.salary}!")
+
+    @classmethod
+    def update(cls):
+        print(f"The salary of Ankur is {cls.salary}")
+
+Ankur = Worker()
+Ankur.salary = 15000
+
+print(Ankur.show())     # This will print the salary of Ankur on the basis of instance variable
+print(Ankur.update())   # This will print the salary of Ankur on the basis of class variable
